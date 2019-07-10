a1c577399bdf03509d6d6bafad3e96a743c10a1d
Benoit Rion <benoitrion@gmail.com>
2019-07-10 10:30:42 +0200

    Fix css rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,56 @@
 import App, { Container } from "next/app";
 import Head from "next/head";
+import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../theme";
 
 export default class Woodify extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-        return { pageProps };
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    renderHead() {
-        return (
-            <Head>
-                <title>Woodify</title>
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-                />
-                <link rel="icon" type="image/x-icon" href="static/images/favicon.ico" />
+    return { pageProps };
+  }
 
-            </Head>
-        );
-    }
+  renderHead() {
+    return (
+      <Head>
+        <title>Woodify</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <link rel="icon" type="image/x-icon" href="static/images/favicon.ico" />
+      </Head>
+    );
+  }
 
-    render() {
-        const { Component, pageProps } = this.props;
-        return (
-            <Container>
-                {this.renderHead()}
-                <CssBaseline />
-                <Component {...pageProps} />
-            </Container>
-        );
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        {this.renderHead()}
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
 }
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
-    const server = express()
+    const server = express();
 
-    server.get('/parcel/:id', (req, res) => {
-        const actualPage = '/parcel'
-        const queryParams = { id: req.params.id }
-        app.render(req, res, actualPage, queryParams)
-    })
-  
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
+    server.get("/parcel/:id", (req, res) => {
+      const actualPage = "/parcel";
+      const queryParams = { id: req.params.id };
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
     server.listen(3009, err => {
-      if (err) throw err
-      console.log('> Ready on http://localhost:3009')
-    })
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3009");
+    });
   })
   .catch(ex => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+    console.error(ex.stack);
+    process.exit(1);
+  });
